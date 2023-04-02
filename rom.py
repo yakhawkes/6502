0ce1e9314caa6a349c5e006033b307753c27245b
Yak <yakhawkes@gmail.com>
2023-04-02 16:22:40 +0200

    Xylon LEDs in hex

---
+++ rom.py
@@ -0,0 +1,23 @@
+code = bytearray([
+    0xa9, 0xff,          # lda #ff
+    0x8d, 0x02, 0x60,    # sta $6002
+
+    0xa9, 0x01,          # lda #01
+
+    0x8d, 0x00, 0x60,    # sta $6000
+    0x2a,                # rol
+    0xf0, 0x03,          # beq $03
+    0x4c, 0x07, 0x80,    # jmp $8007
+    0x8d, 0x00, 0x60,    # sta $6000
+    0x6a,                # ror
+    0xf0, 0xf1,          # beq $f1
+    0x4c, 0x10, 0x80     # jmp $8010
+])
+
+rom = code +  bytearray([0xea] * (32768 - len(code)))
+
+rom[0x7ffc] = 0x00
+rom[0x7ffd] = 0x80
+
+with open("rom.bin", "wb") as out_file:
+    out_file.write(rom);
