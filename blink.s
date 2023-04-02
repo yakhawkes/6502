02c3270c87f972b2d1aaff1837decb6a5bcd8fad
Yak <yakhawkes@gmail.com>
2023-04-02 18:57:40 +0200

    alt blink from eater

---
+++ blink.s
@@ -0,0 +1,18 @@
+    .org $8000
+reset:
+    lda #$ff
+    sta $6002
+
+loop:
+    lda #$55
+    sta $6000
+
+    lda #$aa
+    sta $6000
+
+    jmp loop
+
+
+    .org $fffc
+    .word reset
+    .word $0000
